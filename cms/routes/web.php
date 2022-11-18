95d6e5aaec93de921843fa17be0e6fd6af0c8dd1
shusuke919 <sh091965@gmail.com>
2022-11-18 16:48:09 +0900

    react構築

--- cms/routes/web.php
+++ cms/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//初期設定
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('{any}', function () {
+    return view('app');
+})->where('any','.*');
